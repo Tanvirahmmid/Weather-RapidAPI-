cdf199730aa8b5b9e16f5b1ad47a5aba7847a583
Tanvir Ahmmid <106543545+Tanvirahmmid@users.noreply.github.com>
2024-05-05 04:32:36 -0700

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherInterface } from './models/weather.model';
+import { WeatherAPIService } from './services/weather-api.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherAPIService) {}
+
+  weatherData?: WeatherInterface;
+  cityName: string = '';
+
+  fetchData() {
+    this.getWeather(this.cityName);
+    this.cityName = '';
+  }
+
+  ngOnInit(): void {
+    this.getWeatherByGeoLocation();
+  }
+
+  private getWeather(cityName: string) {
+    this.weatherService.fetchWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+      },
+    });
+  }
+
+  private getWeatherByGeoLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let long = position.coords.longitude;
+        let lat = position.coords.latitude;
+        this.weatherService.getlocation(lat, long).subscribe({
+          next: (res) => {
+            this.getWeather(res.city);
+            this.cityName = '';
+          },
+        });
+      });
+    }
+  }
+}
